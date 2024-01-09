c822e6cf0f2579c78c2e85252df7bf08cd74a4a9
Reahana <reahana233@gmail.com>
2024-01-09 12:53:18 +0600

    all

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <p>DevTech © 2023 , All Rights Are Reserved</p>
+        </div>
+    );
+};
+
+export default Footer;
