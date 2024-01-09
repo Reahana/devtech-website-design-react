912ffe0da1828090c4f868134096985c8186cd73
Reahana <reahana233@gmail.com>
2024-01-09 12:54:29 +0600

    setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 import Footer from './Components/Footer/Footer'
 import Nav from './Components/Nav/Nav'
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
     <>
